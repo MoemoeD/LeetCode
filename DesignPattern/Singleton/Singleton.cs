9f33f9cc1f24de97c719dffac8c869d7014c0b93
MoemoeD <37505963+MoemoeD@users.noreply.github.com>
2019-09-22 14:37:33 +0800

    Singleton

---
+++ DesignPattern/Singleton/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Singleton
+{
+    public class Singleton
+    {
+        private static Singleton _singleton;
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton Instance()
+        {
+            if (_singleton == null)
+            {
+                _singleton = new Singleton();
+            }
+            return _singleton;
+        }
+
+    }
+}
