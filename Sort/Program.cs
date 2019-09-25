58adbe91d54a87ac763a74171a7dde5fe63e0262
MoemoeD <37505963+MoemoeD@users.noreply.github.com>
2019-09-25 22:32:59 +0800

    快速排序

--- Sort/Program.cs
+++ Sort/Program.cs
@@ -11,7 +11,7 @@ namespace Sort
         static void Main(string[] args)
         {
             int[] input = new int[] { 53, 25, 99, 22, 21, 5, 63, 46 };
-            int[] output = InsertionSort(input);
+            int[] output = QuickSort(input);
 
             foreach (var i in output)
             {
@@ -76,5 +76,47 @@ namespace Sort
             return input;
         }
 
+        public static int[] QuickSort(int[] input)
+        {
+            qs(input, 0, input.Length - 1);
+
+            return input;
+        }
+
+        public static void qs(int[] input, int l, int r)
+        {
+            if (l < r)
+            {
+                int i = l;
+                int j = r;
+                int num = input[l];
+                while (i < j)
+                {
+                    while (i < j && num < input[j])
+                    {
+                        j--;
+                    }
+                    if (i < j)
+                    {
+                        input[i] = input[j];
+                        i++;
+                    }
+
+                    while (i < j && num > input[i])
+                    {
+                        i++;
+                    }
+                    if (i < j)
+                    {
+                        input[j] = input[i];
+                        j--;
+                    }
+                }
+                input[i] = num;
+
+                qs(input, l, i - 1);
+                qs(input, i + 1, r);
+            }
+        }
     }
 }
